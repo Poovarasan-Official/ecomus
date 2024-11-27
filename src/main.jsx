e2cb7e6d3b9101d0e3ed2ba89bfded9417279d68
Poovarasan <poovarasan.official@gmail.com>
2024-11-27 16:36:15 +0530

    Updated: Css

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ThemeProvider } from "@mui/material/styles";
-import Theme from "./styles/theme";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={Theme}>
+  <StrictMode>
     <App />
-  </ThemeProvider>
+  </StrictMode>
 );
